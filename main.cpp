374024b05ffc11d3d6d16c8ea7c41b3044317fca
Gordon McGaffin <gordon.t.mcgaffin@gmail.com>
2026-03-23 15:21:46 +0200

    Sandy box

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+// Created by gordon on 3/23/26.
+//
+
+#include "main.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int my_age = 500;
+    cout << "Hello world, this is c++";
+    cout << "\n" << my_age;
+    return 0;
+
+}
---
+++ main.h
@@ -0,0 +1,13 @@
+//
+// Created by gordon on 3/23/26.
+//
+
+#ifndef CPP_SANDBOX_MAIN_H
+#define CPP_SANDBOX_MAIN_H
+
+
+class main {
+};
+
+
+#endif //CPP_SANDBOX_MAIN_H
